bbb0e590fa856ca4ed1edab22dd1ea09456a7eb8
adiletgov <astanadilet@gmail.com>
2025-08-10 12:19:49 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["flowbite.s3.amazonaws.com", "images.unsplash.com"],
   },
+  output: 'export', // for Next.js 13+ App Router
+  basePath: '/moon-restaurant', // repo name
+  assetPrefix: '/moon-restaurant/', // repo name
 };
 
 module.exports = nextConfig;
